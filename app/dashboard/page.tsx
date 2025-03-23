d82b5ddd2cbd0a0368e19adc384a60a794742120
Kartik Dhomne <kartik.dhomne@acceleratebs.com>
2025-03-23 17:37:14 +0530

    fixes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -36,13 +36,7 @@ export default async function MyPostsRoute() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {data.map((item) => (
-          <BlogPostCard
-            data={{
-              ...item,
-              authorImage: item.authorImage || undefined, // Convert null to undefined
-            }}
-            key={item.id}
-          />
+          <BlogPostCard data={item} key={item.id} />
         ))}
       </div>
     </div>
